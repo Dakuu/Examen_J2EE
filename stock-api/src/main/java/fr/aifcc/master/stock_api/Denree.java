c28146ffb8862f65dd99e4a7db27cc3a81b3bcaf
dakuu <ch.richard11@gmail.com>
2017-06-13 09:39:28 +0200

    Ajout de Item et Denree

---
+++ stock-api/src/main/java/fr/aifcc/master/stock_api/Denree.java
@@ -0,0 +1,21 @@
+package fr.aifcc.master.stock_api;
+
+public class Denree extends Item {
+	
+	private String nom;
+	private String categorie;
+	
+	public String getNom(){
+		return this.nom;
+	}
+	public void setNom(String nom){
+		this.nom = nom;
+	}
+	
+	public String getCategorie){
+		return this.categorie;
+	}
+	public void setCategorie(String categorie){
+		this.categorie = categorie;
+	}
+}
---
+++ stock-api/src/main/java/fr/aifcc/master/stock_api/Item.java
@@ -0,0 +1,15 @@
+package fr.aifcc.master.stock_api;
+
+public abstract class Item
+{
+	private long id;
+	
+	public long getId()
+	{
+		return this.id;
+	}
+	public void setId(long id)
+	{
+		this.id = id;
+	}
+}
